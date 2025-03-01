aa8657e804be67564019995a80c2938c392c8f86
dan0102dan <me@dan0102dan.ru>
2025-03-01 18:56:32 +0300

    раздена логика ядра и API

--- backend/internal/app/group.go
+++ backend/internal/app/group.go
@@ -1,4 +1,4 @@
-package magitrickle
+package app
 
 import (
 	"errors"
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	"magitrickle/models"
-	"magitrickle/netfilter-helper"
+	netfilterHelper "magitrickle/netfilter-helper"
 
 	"github.com/rs/zerolog/log"
 	"github.com/vishvananda/netlink"
@@ -26,6 +26,17 @@ type Group struct {
 	ipsetToLink *netfilterHelper.IPSetToLink
 }
 
+func (g *Group) Enabled() bool {
+	panic("unimplemented")
+}
+
+func NewGroup(group *models.Group, app *App) (*Group, error) {
+	return &Group{
+		Group: group,
+		app:   app,
+	}, nil
+}
+
 func (g *Group) addIP(address net.IP, ttl uint32) error {
 	return g.ipset.AddIP(address, &ttl)
 }
@@ -33,11 +44,9 @@ func (g *Group) addIP(address net.IP, ttl uint32) error {
 func (g *Group) AddIP(address net.IP, ttl uint32) error {
 	g.locker.Lock()
 	defer g.locker.Unlock()
-
 	if !g.enabled.Load() {
 		return nil
 	}
-
 	return g.addIP(address, ttl)
 }
 
@@ -48,11 +57,9 @@ func (g *Group) delIP(address net.IP) error {
 func (g *Group) DelIP(address net.IP) error {
 	g.locker.Lock()
 	defer g.locker.Unlock()
-
 	if !g.enabled.Load() {
 		return nil
 	}
-
 	return g.delIP(address)
 }
 
@@ -63,11 +70,9 @@ func (g *Group) listIPs() (map[string]*uint32, error) {
 func (g *Group) ListIPs() (map[string]*uint32, error) {
 	g.locker.Lock()
 	defer g.locker.Unlock()
-
 	if !g.enabled.Load() {
 		return nil, nil
 	}
-
 	return g.listIPs()
 }
 
@@ -85,7 +90,6 @@ func (g *Group) deleteIPSet() error {
 	if g.ipset == nil {
 		return nil
 	}
-
 	err := g.ipset.Disable()
 	if err != nil {
 		return fmt.Errorf("failed to destroy ipset: %w", err)
@@ -108,10 +112,9 @@ func (g *Group) unlinkIfaceFromIPSet() error {
 	if g.ipsetToLink == nil {
 		return nil
 	}
-
 	err := g.ipsetToLink.Disable()
 	if err != nil {
-		return fmt.Errorf("failed to unlink ipset to interface: %w", err)
+		return fmt.Errorf("failed to unlink ipset from interface: %w", err)
 	}
 	g.ipsetToLink = nil
 	return nil
@@ -154,37 +157,27 @@ func (g *Group) enable() error {
 	if !g.enabled.CompareAndSwap(false, true) {
 		return nil
 	}
-
-	err := g.createIPSet()
-	if err != nil {
+	if err := g.createIPSet(); err != nil {
 		return err
 	}
-
-	err = g.linkIfaceToIPSet()
-	if err != nil {
+	if err := g.linkIfaceToIPSet(); err != nil {
 		return err
 	}
-
 	if g.FixProtect {
-		err = g.fixProtection()
-		if err != nil {
+		if err := g.fixProtection(); err != nil {
 			return err
 		}
 	}
-
 	return nil
 }
 
 func (g *Group) Enable() error {
 	g.locker.Lock()
 	defer g.locker.Unlock()
-
-	err := g.enable()
-	if err != nil {
-		g.disable()
+	if err := g.enable(); err != nil {
+		_ = g.disable()
 	}
-
-	return err
+	return nil
 }
 
 func (g *Group) disable() error {
@@ -192,7 +185,6 @@ func (g *Group) disable() error {
 		return nil
 	}
 	defer g.enabled.Store(false)
-
 	var errs []error
 	if g.FixProtect {
 		errs = append(errs, g.unfixProtection())
@@ -205,28 +197,23 @@ func (g *Group) disable() error {
 func (g *Group) Disable() error {
 	g.locker.Lock()
 	defer g.locker.Unlock()
-
 	return g.disable()
 }
 
 func (g *Group) Sync() error {
 	g.locker.Lock()
 	defer g.locker.Unlock()
-
 	now := time.Now()
-
 	addresses := make(map[string]uint32)
 	knownDomains := g.app.records.ListKnownDomains()
 	for _, domain := range g.Rules {
 		if !domain.IsEnabled() {
 			continue
 		}
-
 		for _, domainName := range knownDomains {
 			if !domain.IsMatch(domainName) {
 				continue
 			}
-
 			domainAddresses := g.app.records.GetARecords(domainName)
 			for _, address := range domainAddresses {
 				ttl := uint32(now.Sub(address.Deadline).Seconds())
@@ -236,63 +223,44 @@ func (g *Group) Sync() error {
 			}
 		}
 	}
-
 	currentAddresses, err := g.listIPs()
 	if err != nil {
 		return fmt.Errorf("failed to get old ipset list: %w", err)
 	}
-
 	for addr, ttl := range addresses {
-		if _, exists := currentAddresses[addr]; exists {
-			if currentAddresses[addr] == nil {
+		if currTTL, exists := currentAddresses[addr]; exists {
+			if currTTL == nil {
 				continue
 			} else {
-				if ttl < *currentAddresses[addr] {
+				if ttl < *currTTL {
 					continue
 				}
 			}
 		}
 		ip := net.IP(addr)
-		err = g.addIP(ip, ttl)
-		if err != nil {
-			log.Error().
-				Str("address", ip.String()).
-				Err(err).
-				Msg("failed to add address")
+		if err := g.addIP(ip, ttl); err != nil {
+			log.Error().Str("address", ip.String()).Err(err).Msg("failed to add address")
 		} else {
-			log.Trace().
-				Str("address", ip.String()).
-				Err(err).
-				Msg("add address")
+			log.Trace().Str("address", ip.String()).Msg("added address")
 		}
 	}
-
 	for addr := range currentAddresses {
 		if _, ok := addresses[addr]; ok {
 			continue
 		}
 		ip := net.IP(addr)
-		err = g.delIP(ip)
-		if err != nil {
-			log.Error().
-				Str("address", ip.String()).
-				Err(err).
-				Msg("failed to delete address")
+		if err := g.delIP(ip); err != nil {
+			log.Error().Str("address", ip.String()).Err(err).Msg("failed to delete address")
 		} else {
-			log.Trace().
-				Str("address", ip.String()).
-				Err(err).
-				Msg("del address")
+			log.Trace().Str("address", ip.String()).Msg("deleted address")
 		}
 	}
-
 	return nil
 }
 
 func (g *Group) NetfilterDHook(iptType, table string) error {
 	g.locker.Lock()
 	defer g.locker.Unlock()
-
 	if g.enabled.Load() {
 		if g.FixProtect && table == "filter" {
 			if iptType == "" || iptType == "iptables" {
@@ -312,23 +280,13 @@ func (g *Group) NetfilterDHook(iptType, table string) error {
 				}
 			}
 		}
-
 		return g.ipsetToLink.NetfilterDHook(iptType, table)
 	}
-
 	return nil
 }
 
 func (g *Group) LinkUpdateHook(event netlink.LinkUpdate) error {
 	g.locker.Lock()
 	defer g.locker.Unlock()
-
 	return g.ipsetToLink.LinkUpdateHook(event)
-}
-
-func NewGroup(group *models.Group, app *App) (*Group, error) {
-	return &Group{
-		Group: group,
-		app:   app,
-	}, nil
 }
